d3e4c7eb8b9ab6fc4ab32587a9c7a02f3555e975
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-06-22 13:05:45 +0300

    Product Detail ve Image Dtoları

--- Services/Catalog/MultiShop.Catalog/Entities/ProductImage.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductImage.cs
@@ -1,12 +1,20 @@
-﻿namespace MultiShop.Catalog.Entities
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
 {
     public class ProductImage
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string ProductImageID { get; set; }
         public string Images1{ get; set; }
         public string Images2 { get; set; }
         public string Images3 { get; set; }
         public string ProductID { get; set; }
+
+        [BsonIgnore]
         public Product Product { get; set; }
+
     }
 }
