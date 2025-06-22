3c9d204a33e2a7a20b87c037b685e6d476f08b0d
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-06-22 14:34:46 +0300

    Product Service Metotları

---
+++ Services/Catalog/MultiShop.Catalog/Services/ProductServices/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using MultiShop.Catalog.Dtos.ProductDtos;
+
+namespace MultiShop.Catalog.Services.ProductServices
+{
+    public interface IProductService
+    {
+        Task<List<ResultProductDto>> GetAllCategoriesAsync();
+        Task CreateProductAsync(CreateProductDto createProductDto);
+
+        Task UpdateProductAsync(UpdateProductDto updateProductDto);
+
+        Task DeleteProductAsync(string id);
+
+        Task<GetByIDProductDto> GetProductByIdAsync(string id);
+    }
+}
