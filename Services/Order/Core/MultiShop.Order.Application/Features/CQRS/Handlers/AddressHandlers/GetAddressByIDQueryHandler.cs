8d39b4f10140300b6dcd06c8105eab03c8fc868f
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-06-24 15:11:34 +0300

    Addressleri Listeleme ve Id'ye Göre Getirme Handle Metotları

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/GetAddressByIDQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Queries.AddressQueries;
+using MultiShop.Order.Application.Features.CQRS.Results.AddressResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class GetAddressByIDQueryHandler
+    {
+        private readonly IRepository<Address> _repository;
+
+        public GetAddressByIDQueryHandler(IRepository<Address> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetAddressByIDQueryResult> Handle(GetAdressByIDQuery query)
+        {
+            var values = await _repository.GetByIdAsync(query.Id);
+            return new GetAddressByIDQueryResult
+            {
+                AddressID = values.AddressID,
+                UserID = values.UserID,
+                District = values.District,
+                City = values.City,
+                Detail = values.Detail
+            };
+        }
+    }
+}
