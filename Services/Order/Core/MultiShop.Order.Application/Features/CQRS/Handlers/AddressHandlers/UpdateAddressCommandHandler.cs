7c52f7860cfb05727ad35e30a585d18e16ed92b6
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-06-24 15:16:40 +0300

    Address Remove Update Handler Metotları

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/UpdateAddressCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class UpdateAddressCommandHandler
+    {
+        private readonly IRepository<Address> _repository;
+
+        public UpdateAddressCommandHandler(IRepository<Address> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateAddressCommand updateAddressCommand)
+        {
+            var address = await _repository.GetByIdAsync(updateAddressCommand.AddressID);
+            if (address != null)
+            {
+                address.City = updateAddressCommand.City;
+                address.District = updateAddressCommand.District;
+                address.Detail = updateAddressCommand.Detail;
+                address.UserID = updateAddressCommand.UserID;
+                await _repository.UpdateAsync(address);
+            }
+            else
+            {
+                throw new Exception("Address not found");
+            }
+        }
+    }
+}
