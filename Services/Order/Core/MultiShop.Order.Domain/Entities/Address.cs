a8416150f5e5d2e3491c60579e8e92a6ca55cb7d
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-06-24 13:51:03 +0300

    Order Domain Katmanı

--- Services/Order/Core/MultiShop.Order.Domain/Entities/Address.cs
+++ Services/Order/Core/MultiShop.Order.Domain/Entities/Address.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace MultiShop.Order.Domain.Entities
 {
-    internal class Address
+    public class Address
     {
+        public int AddressID { get; set; }
+        public string UserID { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string Detail { get; set; }
     }
 }
