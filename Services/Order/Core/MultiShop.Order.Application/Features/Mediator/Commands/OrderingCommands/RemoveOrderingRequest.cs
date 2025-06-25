7583d55a1edf8d3caca8ab1f7aea25a010ae8168
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-06-25 12:45:52 +0300

    Mediator ile Ordering Sınıfı İşlemleri

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Commands/OrderingCommands/RemoveOrderingRequest.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.Mediator.Commands.OrderingCommands
+{
+    public class RemoveOrderingRequest:IRequest
+    {
+        public int Id { get; set; }
+        public RemoveOrderingRequest(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Queries/OrderingQueries/GetOrderingByIDQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using MultiShop.Order.Application.Features.Mediator.Results.OrderingResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.Mediator.Queries.OrderingQueries
+{
+    public class GetOrderingByIDQuery:IRequest<GetOrderingByIDQueryResult>
+    {
+        public int Id { get; set; }
+        public GetOrderingByIDQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
