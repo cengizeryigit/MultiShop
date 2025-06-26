35d7c3fc392c19eff5081389b8c8e14e9edc065a
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-06-26 14:58:34 +0300

    Configde Admin ve Manager Kullanıcıları

--- IdentityServer/MultiShop.IdentityServer/Config.cs
+++ IdentityServer/MultiShop.IdentityServer/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -42,6 +43,32 @@ namespace MultiShop.IdentityServer
                 AllowedGrantTypes = GrantTypes.ClientCredentials,
                 ClientSecrets ={new Secret("multishopsecret".Sha256())},
                 AllowedScopes ={ "CatalogReadPermission" }
+            },
+
+            //Manager
+            new Client()
+            {
+                ClientId = "MultiShopManagerID",
+                ClientName = "Multi Shop Manager User",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets ={new Secret("multishopsecret".Sha256())},
+                AllowedScopes = { "CatalogReadPermission", "CatalogFullPermission" }
+            },
+
+            //Admin
+            new Client()
+            {
+                ClientId = "MultiShopAdminID",
+                ClientName = "Multi Shop Admin User",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets ={new Secret("multishopsecret".Sha256())},
+                AllowedScopes = { "CatalogReadPermission", "CatalogFullPermission", "DiscountFullPermission", "OrderFullPermission",
+                    IdentityServerConstants.LocalApi.ScopeName,
+                    IdentityServerConstants.StandardScopes.Email,
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile
+                },
+                AccessTokenLifetime = 600
             }
         };
     }
