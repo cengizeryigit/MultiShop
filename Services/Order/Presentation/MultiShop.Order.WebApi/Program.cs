2a6443db7b41bc3bb9623dcff2f85f5ab5ee7213
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-06-26 17:13:49 +0300

    Order Mikroservisinin Token ile Korunması

--- Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
 using MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
 using MultiShop.Order.Application.Interfaces;
@@ -7,6 +8,13 @@ using MultiShop.Order.Persistence.Repositories;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    opt.Authority = builder.Configuration["IdentityServerUrl"];
+    opt.Audience = "ResourceOrder";
+    opt.RequireHttpsMetadata = false;
+});
 
 // Add services to the container.
 builder.Services.AddDbContext<OrderContext>();
@@ -44,6 +52,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
