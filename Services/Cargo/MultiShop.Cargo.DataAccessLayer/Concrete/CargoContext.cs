a0c369a1edb57db8c111f7dc74fa385fd77e9e4c
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-07-02 16:44:23 +0300

    CargoContext Sınıfı

---
+++ Services/Cargo/MultiShop.Cargo.DataAccessLayer/Concrete/CargoContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Cargo.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.DataAccessLayer.Concrete
+{
+    public class CargoContext :DbContext
+    {
+        override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost,1441;initial Catalog=MultiShopCargoDb;User=sa;Password=#Kellepaca1997");
+        }
+        public DbSet<CargoCompany> CargoCompanies { get; set; }
+        public DbSet<CargoDetail> CargoDetails { get; set; }
+        public DbSet<CargoCustomer> CargoCustomers { get; set; }
+        public DbSet<CargoOperations> CargoOperations { get; set; }
+    }
+}
