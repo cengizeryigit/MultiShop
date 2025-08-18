b27e42e3ea48b06baab4a2456995ec2e772a8f88
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-08-19 02:43:31 +0300

    Ürün Listesinin Tamamlanması

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductListViewComponents/_ProductListComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductListViewComponents
+{
+    public class _ProductListComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            // You can pass any model or data to the view here if needed
+            return View();
+        }
+    }
+}
