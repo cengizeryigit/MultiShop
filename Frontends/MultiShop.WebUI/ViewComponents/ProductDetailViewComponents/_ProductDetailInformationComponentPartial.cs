4d63852837d7500bfee8686163fd335c94324d89
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-08-19 16:12:14 +0300

    Product Detail Component Partial

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_ProductDetailInformationComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductDetailViewComponents
+{
+    public class _ProductDetailInformationComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            // You can pass any model or data to the view if needed
+            return View();
+        }
+
+    }
+}
