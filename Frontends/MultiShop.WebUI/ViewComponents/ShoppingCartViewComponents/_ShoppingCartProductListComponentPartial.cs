bfb65b6c50976e0d1fbfec9a81a52455fdf7f4d0
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-08-19 16:25:59 +0300

    Shopping Cart

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ShoppingCartViewComponents/_ShoppingCartProductListComponentPartial.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ShoppingCartViewComponents
+{
+    public class _ShoppingCartProductListComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            // Here you can retrieve the shopping cart items from a service or database
+            // For demonstration, we will return a simple view
+            return View();
+        }
+
+    }
+}
